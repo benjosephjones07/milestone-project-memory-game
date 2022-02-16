dec6d65075367c7938657c18f381520e6c09c114
benjosephjones07 <benjosephjones07@gmail.com>
2022-02-16 10:49:36 +0000

    code lives counter and starter lose game screen

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,21 @@ let hasRevealedCard = false;
 let matchedCards = []
 let totalCards = 16;
 
+// shuffle cards
+const colorsInGame = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'indigo',
+  'violet',
+  'brown'
+];
+document.addEventListener('DOMContentLoaded', () => {
+  Array.from(cards).sort(() => 0.5 - Math.random());
+})
+
 function cardReveal() {
   // removes double click bug//
   if (this === firstReveal) return;
@@ -29,10 +44,15 @@ function cardReveal() {
       secondReveal.removeEventListener('click', cardReveal);
     } else {
       function flipback() {
+        lives--;
+        livesCounter.textContent = lives;
         firstReveal.classList.add('unflipped');
         secondReveal.classList.add('unflipped');
       } setTimeout(flipback, 500);
     }
+  }
+  if (lives == 0){
+    alert('Sorry, you have lost the game. Please try again!');
   }
 }
 
